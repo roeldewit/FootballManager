177542dd160ac9dbbfad9829cc4098cfb446fa3d
Roel de Wit <rthdewit@gmail.com>
2015-01-16 15:31:23 +0100

    Setup EntityFramework

---
+++ RoeldeWit.FootballManager.SqlDataAccess/Classes/FootballContext.cs
@@ -0,0 +1,45 @@
+﻿using RoeldeWit.FootballManager.Domain.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace RoeldeWit.FootballManager.SqlDataAccess.Classes
+{
+    /// <summary>
+    /// DbContext for FootballManager
+    /// </summary>
+    public class FootballContext : DbContext
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public FootballContext()
+            : base("FootballContext")
+        {
+
+        }
+
+        /// <summary>
+        /// DbSet for clubs
+        /// </summary>
+        public DbSet<Club> Clubs { get; set; }
+
+        /// <summary>
+        /// DbSet for countries
+        /// </summary>
+        public DbSet<Country> Countries { get; set; }
+
+        /// <summary>
+        /// DbSet for leagues
+        /// </summary>
+        public DbSet<League> Leagues { get; set; }
+
+        /// <summary>
+        /// Method that's called when the model is created
+        /// </summary>
+        /// <param name="modelBuilder">DbModelBuilder</param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ RoeldeWit.FootballManager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoeldeWit.FootballManager.Web.MvcApplication" Language="C#" %>
